e8381fdd8da267be84e5be1b1f9686326c165840
Abdallah <Abdollah.kohail@gmail.com>
2023-09-11 14:14:12 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /*
  *hfh
  * this
  */
+
+
 int main(void)  /*hok*/
 {
-puts("\"Programming is like building a multilingual puzzle");  /*this is*/
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");  /*this is*/
+	return (0);
 }
