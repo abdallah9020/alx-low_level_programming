afc89d6c9c220c0adcd784cda9d98f27485cdd9e
Abdallah <Abdollah.kohail@gmail.com>
2023-09-13 09:53:46 -0700

    d

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	char alpha = 'a';
+
+	for (i = 1; i < 27; alpha++, i++)
+		putchar(alpha);
+	alpha = 'A';
+	for (i = 1; i < 27; alpha++, i++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
