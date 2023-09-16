fd92876097ed9d2c1d96542201c615c4a1834be4
Abdallah <Abdollah.kohail@gmail.com>
2023-09-16 13:17:11 -0700

    f

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,12 @@
 
 int print_last_digit(int n)
 {
-	if (n == INT_MIN)
-		n = 18;  /* da7akt 3aleih haha */
+	int last;
+
 	if (n < 0)
-		n = -1 * n;
+		last = n % 10;
+		last = -1 * last;
+		_putchar('0' + last);
 	_putchar('0' + (n % 10));
 	return (n % 10);
 }
