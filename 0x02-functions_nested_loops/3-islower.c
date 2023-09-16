3765161b4458b1cf6b110b0c7ac23b67997d81e7
Abdallah <Abdollah.kohail@gmail.com>
2023-09-16 10:14:08 -0700

    f

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 /**
  * _islower - enter
  *
+ *@c : parameter c is an int
+ *
  * Return: 1 if lower and 0 if not lower
 */
 
-int _islower(int c) /*parameter c is an integer*/
+int _islower(int c)
 {
 	if ('a' <= c && c <= 'z')
 		return (1);
