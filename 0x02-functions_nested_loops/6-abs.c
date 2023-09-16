8f5b2474d849a8ecda650a2c12e5202ad66f658d
Abdallah <Abdollah.kohail@gmail.com>
2023-09-16 12:27:17 -0700

    f

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+/**
+ * _abs - return the abs value
+ *
+ * @n : number
+ *
+ * Return: abs value
+ */
+
+int _abs(int n)
+{
+		if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+
+}
