2bb5bc6946f252f0f2c8efad1e8d512855d52258
Abdallah <Abdollah.kohail@gmail.com>
2023-09-16 11:51:50 -0700

    f

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - enter
+ *
+ * @n : parameter c is an int
+ *
+ * Return: 1 if +ve , 0 if 0 , -1 if -ve
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
