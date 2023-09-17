63d3dc093808c3b898a4a12dd04910b6f8bfffb5
Abdallah <Abdollah.kohail@gmail.com>
2023-09-17 01:56:10 -0700

    d

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - enter
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	long i;
+	long back = 1;
+	long back2 = 0;
+	int x;
+
+	for (x = 1; x <= 98; x++)
+	{
+		i = back + back2;
+		if (x < 98)
+			printf("%ld, ", i);
+		else
+			printf("%ld", i);
+		back2 = back;
+		back = i;
+	}
+	printf("\n");
+	return (0);
+}
