074caa5574dc1b5a541ae8e585aec628e44ff3a3
Abdallah <Abdollah.kohail@gmail.com>
2023-09-16 22:11:49 -0700

    d

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - entry
- *
+ * @n : n
  * Return: void
 */
 
@@ -12,7 +12,7 @@ void print_times_table(int n)
 	int y;
 	int product;
 	int middle;
-	
+
 	if (n < 0 || n > 15)
 		return;
 
