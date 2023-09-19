498418658a755ddacb1d93c4aeb8dff841a19e84
Abdallah <Abdollah.kohail@gmail.com>
2023-09-19 04:50:16 -0700

    f

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,14 @@ void print_rev(char *s)
 	int j;
 	int k;
 	int lenght = 0;
+	char temp[10000000];
 
 	for (j = 0; s[j] != '\0'; j++)
 		lenght += 1;
 	char temp[lenght];
 
 	for (i = lenght - 1, k = 0; i != 0, k < lenght; i--, k++)
-	{
 		temp[k] = s[i];
-	}
 	for (i = 0; i < lenght; i++)
 		s[i] = temp[i];
 }
