17e0dc2e46e053bc510a62e348d035717e3ac862
Abdallah <Abdollah.kohail@gmail.com>
2023-09-19 04:10:21 -0700

    f

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - enter
+ * @s: string
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+	int k;
+	int lenght = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+		lenght += 1;
+
+	for (i = lenght - 1, k = 0; i >= 0, k < lenght; i--, k++)
+	{
+		temp[k] = s[i];
+	}
+	for (i = 0; i < lenght; i++)
+		s[i] = temp[i];
+}
