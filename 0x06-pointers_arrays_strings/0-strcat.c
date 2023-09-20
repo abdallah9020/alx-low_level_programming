f4525bfbebc656290d244daf290ff7565489bd16
Abdallah <Abdollah.kohail@gmail.com>
2023-09-20 12:25:21 -0700

    f

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+/**
+ * _strcat - enter
+ * @dest: string
+ * @src: string
+ * Return: dest sting
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int lenght1 = strlen(dest);
+	int lenght2 = strlen(src);
+
+	for (i = lenght1, j = 0; j <= lenght2; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
