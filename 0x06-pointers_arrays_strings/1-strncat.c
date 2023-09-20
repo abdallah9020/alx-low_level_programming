100496ebc8a8b5f93aad9fd391bf3fb1366533ad
Abdallah <Abdollah.kohail@gmail.com>
2023-09-20 12:36:50 -0700

    f

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+
+/**
+ * _strncat - enter
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: dest sting
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	int lenght1 = strlen(dest);
+
+	for (i = lenght1, j = 0; j < n; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i + 1] = '\n';
+	return (dest);
+}
