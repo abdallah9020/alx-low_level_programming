f70a5095074ddcc2242e5e57e6510b9ef517edc1
Abdallah <Abdollah.kohail@gmail.com>
2023-09-21 05:24:28 -0700

    f

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,23 @@
-#include "main.h"
 #include <string.h>
 
 /**
- * _strcpy - enter
+ * _strncpy - enter
  * @dest: string
  * @src: string
- * Return: dest sting
+ * @n: int
+ * Return: dest string
 */
 
-char *_strcpy(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int lenght = strlen(src);
+	int lenght = strlen(dest);
 
-	for (i = 0; i <= lenght; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
