015c1c4e91739fe96426e1c711ca123b7575f360
Abdallah <Abdollah.kohail@gmail.com>
2023-09-21 21:25:11 -0700

    f

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+/**
+ * string_toupper - enter
+ * @c: c
+ * Return: string
+*/
+
+char *string_toupper(char *c);
+{
+	int i;
+
+	for (i = 0; i < strlen(c); i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = (a[i] - 'a') + 'A'
+	}
+	return (c);
+}
